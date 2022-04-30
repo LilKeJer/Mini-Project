ae768f3c40a5e0a4bae48692fb1625ed2d2678b6
Kevin Jeremia Soaduon Pasaribu <64350998+LilKeJer@users.noreply.github.com>
2022-04-30 22:36:28 +0700

    Add Router Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/home";
+import Album from "./components/album";
+import Tour from "./components/tour";
+import Profile from "./components/profile";
+import Errorpage from "./components/error";
 
 function App() {
   return (
-    <div>
-      <h1>MiniProjectStart</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Album" element={<Album />} />
+        <Route path="/Tour" element={<Tour />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="*" element={<Errorpage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
