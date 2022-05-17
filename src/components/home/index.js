37dff6d4a2cb5244d663e6a9274ac823f0c5ea4a
Kevin Jeremia Soaduon Pasaribu <64350998+LilKeJer@users.noreply.github.com>
2022-05-17 21:36:36 +0700

    Update pagea home

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,27 +1,29 @@
 import React from "react";
 import Foto from "../../assets/image/foto.png";
-import Foto2 from "../../assets/image/1.png";
 import Album from "../../assets/image/Album.jpg";
 import { TabTitle } from "../../utils/GeneralFunctions";
-import "./style.css";
-function index() {
+import "../../assets/styles.css";
+import { useNavigate } from "react-router-dom";
+
+function Index() {
   TabTitle("Home");
+  const navigate = useNavigate();
+
   return (
-    <>
+    <div>
       <div
-        className="container-fluid fontMK "
+        className="container-fluid fontAll"
         style={{
-          // backgroundImage: `url(${Foto2})`,
           backgroundColor: "#E6D031",
           height: "508px",
         }}
       >
-        <div className="row pt-5 text-center ">
-          <div className="col pt-1 ">
+        <div className="row pt-5 text-center fontMK ">
+          <div className="col-12 col-lg-6  pt-1 ">
             <p>MAMANG</p>
             <p>KESBOR</p>
           </div>
-          <div className="col">
+          <div className="col-12 col-lg-6 mb-5">
             <img
               src={Foto}
               alt="Mardiaal"
@@ -32,17 +34,8 @@ function index() {
           </div>
         </div>
         {/* batas */}
-      </div>
-      <div
-        className="container-fluid fontAll"
-        style={{
-          // backgroundImage: `url(${Foto2})`,
-          backgroundColor: "#171717",
-          height: "411px",
-        }}
-      >
         <div className="row  text-center">
-          <div className="col">
+          <div className="col-12 col-lg-6">
             <img
               src={Album}
               alt="Album"
@@ -51,22 +44,29 @@ function index() {
               }}
             />
           </div>
-          <div className="col ">
+          <div className="col-12 col-lg-6">
             <p style={{ fontSize: "64px" }}>DENGARKAN SEKARANG</p>
             <p className="" style={{ fontSize: "54px" }}>
               ALBUM TERBAIK DI TATA SURYA
             </p>
-            <button className="allButton">ALBUM</button>
+            <button
+              className="allButton"
+              onClick={() => {
+                navigate("/Album");
+              }}
+            >
+              ALBUM
+            </button>
           </div>
         </div>
 
-        <div className="row text-center ">
-          <div className="col-6 fontAll2">
+        <div className="row  ">
+          <div className="col-12 col-lg-6 fontAll2 text-center">
             <p style={{ fontSize: "74px" }}>OFFICIAL</p>
             <p style={{ fontSize: "66px" }}>MUSIC</p>
             <p style={{ fontSize: "54px" }}>VIDEO</p>
           </div>
-          <div className="col-6 mt-4   ">
+          <div className="col-12  col-lg-6 mt-4  text-center ">
             <div
               id="carouselExampleDark"
               className="carousel carousel-light slide"
@@ -120,9 +120,9 @@ function index() {
               </div>
             </div>
           </div>
-          <div className="col-6"></div>
-          <div className="col-6">
-            <div className="">
+          <div className="col-12  col-lg-6 text-center"></div>
+          <div className="col-12  col-lg-6 text-center">
+            <div>
               <button
                 type="button"
                 data-bs-target="#carouselExampleDark"
@@ -154,47 +154,78 @@ function index() {
               ></button>
             </div>
           </div>
-          <p className="fontAll2 " style={{ fontSize: "64px" }}>
+          <p className="fontAll2 text-center" style={{ fontSize: "64px" }}>
             TOUR TATA SURYA
           </p>
           <div
-            className="text-center pb-5 fontAll"
+            className=" pb-5 fontAll text-center"
             style={{
-              // backgroundImage: `url(${Foto2})`,
               backgroundColor: "#171717",
             }}
           >
-            <button className="allButton">INFO</button>
-            <p className="pt-4" style={{ fontSize: "48px" }}>
+            <button
+              className="allButton "
+              onClick={() => {
+                navigate("/Tour");
+              }}
+            >
+              INFO
+            </button>
+            <p className="pt-4 text-center" style={{ fontSize: "48px" }}>
               DAPATKAN PEMBERITAHUAN TENTANG
             </p>
-            <p className="pt-4" style={{ fontSize: "48px" }}>
+            <p className="pt-4 text-center" style={{ fontSize: "48px" }}>
               TOUR TATA SURYA
             </p>
-            <div className="row pt-4">
-              <div className="col-5 ps-5">
-                <button className="ms-5 allButton">Register</button>
+          </div>
+          <div className="container  pb-4 text-center">
+            <div className="row pb-4 ">
+              <div className="col-11  col-lg-5 ps-5">
+                <button
+                  onClick={() => {
+                    navigate("/Register");
+                  }}
+                  className="allButton"
+                >
+                  Register
+                </button>
               </div>
-              <div className="col-2">
-                <p style={{ fontSize: "32px" }}>ATAU</p>
+              <div className="col-12 col-lg-2 ">
+                <p clasName="" style={{ fontSize: "32px" }}>
+                  ATAU
+                </p>
               </div>
-              <div className="col-5 pe-5">
-                <button className="me-5 allButton"> LOGIN</button>
+              <div className="col-12  col-lg-4  ">
+                <button
+                  className="allButton"
+                  onClick={() => {
+                    navigate("/Login");
+                  }}
+                >
+                  LOGIN
+                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
+
+      <div
+        className="container fontAll"
+        style={{
+          backgroundColor: "#171717",
+          height: "411px",
+        }}
+      ></div>
       <div
         className="container-fluid"
         style={{
-          // backgroundImage: `url(${Foto2})`,
           backgroundColor: "#E6D031",
           height: "700px",
         }}
       ></div>
-    </>
+    </div>
   );
 }
 
-export default index;
+export default Index;
